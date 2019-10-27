1c99a9652d0e37c904c3f55c8a4ce6d55cff3b8a
Nenad Palinkasevic <nesa993@gmail.com>
2019-10-27 17:00:37 +0100

    Updates

---
+++ precache-manifest.c5e796afc67b9c11ccafdc62139989f7.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = [
+  {
+    "revision": "89889688147bd7575d6327160d64e760",
+    "url": "/CharitySwap/static/media/glyphicons-halflings-regular.89889688.svg"
+  },
+  {
+    "revision": "189ae1c1e9830a3acb88",
+    "url": "/CharitySwap/static/css/main.824734b0.chunk.css"
+  },
+  {
+    "revision": "84aa8dd47f0cf43b69df",
+    "url": "/CharitySwap/static/js/1.84aa8dd4.chunk.js"
+  },
+  {
+    "revision": "a1c77e27c2133f106801",
+    "url": "/CharitySwap/static/js/runtime~main.a1c77e27.js"
+  },
+  {
+    "revision": "448c34a56d699c29117adc64c43affeb",
+    "url": "/CharitySwap/static/media/glyphicons-halflings-regular.448c34a5.woff2"
+  },
+  {
+    "revision": "f4769f9bdb7466be65088239c12046d1",
+    "url": "/CharitySwap/static/media/glyphicons-halflings-regular.f4769f9b.eot"
+  },
+  {
+    "revision": "fa2772327f55d8198301fdb8bcfc8158",
+    "url": "/CharitySwap/static/media/glyphicons-halflings-regular.fa277232.woff"
+  },
+  {
+    "revision": "e18bbf611f2a2e43afc071aa2f4e1512",
+    "url": "/CharitySwap/static/media/glyphicons-halflings-regular.e18bbf61.ttf"
+  },
+  {
+    "revision": "189ae1c1e9830a3acb88",
+    "url": "/CharitySwap/static/js/main.189ae1c1.chunk.js"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/CharitySwap/static/media/fontawesome-webfont3e6e.af7ae505.woff2"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/CharitySwap/static/media/fontawesome-webfont3e6e.fee66e71.woff"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/CharitySwap/static/media/fontawesome-webfont3e6e.674f50d2.eot"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/CharitySwap/static/media/fontawesome-webfontd41d.674f50d2.eot"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/CharitySwap/static/media/fontawesome-webfont3e6e.b06871f2.ttf"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/CharitySwap/static/media/fontawesome-webfont3e6e.912ec66d.svg"
+  },
+  {
+    "revision": "df1f2b89029d10e91b05c2715a4e073a",
+    "url": "/CharitySwap/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/CharitySwap/precache-manifest.c5e796afc67b9c11ccafdc62139989f7.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/CharitySwap/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
